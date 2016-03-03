6290a935b40b27e12240ab5df2892fa383fae5f7
yariv <Yariv543@gmail.com>
2016-03-03 23:48:05 +0200

    Fixed generator script
    changed to output the correct one (nanopb)

--- ArduTank/proto_gen.sh
+++ ArduTank/proto_gen.sh
@@ -3,6 +3,5 @@
 OUT_DIR=./
 
 for file in src/protos/*.proto;
-do nanopb-0.3.5-linux-x86/generator-bin/protoc --cpp_out=$OUT_DIR --python_out=$OUT_DIR $file;
+do nanopb-0.3.5-linux-x86/generator-bin/protoc --nanopb_out=$OUT_DIR $file;
 done;
-
